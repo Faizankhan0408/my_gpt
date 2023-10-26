f45f6199e51bd64f712275789975bb517c3cb6f3
Mohammad Faizan <86049664+Faizankhan0408@users.noreply.github.com>
2023-10-26 09:21:53 +0530

    Update secrets.dart

--- lib/utilities/secrets.dart
+++ lib/utilities/secrets.dart
@@ -1,2 +1,1 @@
-// const openAIAPIKey="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";
-const openAIAPIKey="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";
+const openAIAPIKey="XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX";
